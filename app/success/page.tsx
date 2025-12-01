01bb9a52157dab9532a84c7e08734788aa996d7b
LHMisme420 <lhmisme2011@gmail.com>
2025-12-01 01:55:51 -0500

    Create page.tsx

---
+++ app/success/page.tsx
@@ -0,0 +1,10 @@
+export default function Success() {
+  return (
+    <main className="flex min-h-screen items-center justify-center text-center p-6">
+      <div>
+        <h1 className="text-6xl font-bold mb-8">Done!</h1>
+        <p className="text-3xl">Your postcard is saved.<br />See you in 2035 ❤️</p>
+      </div>
+    </main>
+  );
+}
